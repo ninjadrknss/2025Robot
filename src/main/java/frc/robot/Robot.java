bcf500e95719e9344b72196fcc4243238e4084f4
Alex <alex@petrys.com>
2025-01-08 10:47:51 -0800

    Vendor Deps

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -5,93 +5,68 @@
 
 package frc.robot;
 
+import com.ctre.phoenix6.hardware.TalonFX;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
 
-
-
-public class Robot extends TimedRobot
-{
+public class Robot extends TimedRobot {
     private Command autonomousCommand;
-    
+
     private final RobotContainer robotContainer;
-    
-    
-    public Robot()
-    {
+
+    public Robot() {
         robotContainer = new RobotContainer();
     }
-    
-    
+
     @Override
-    public void robotPeriodic()
-    {
+    public void robotPeriodic() {
         CommandScheduler.getInstance().run();
     }
-    
-    
+
     @Override
     public void disabledInit() {}
-    
-    
+
     @Override
     public void disabledPeriodic() {}
-    
-    
+
     @Override
     public void disabledExit() {}
-    
-    
+
     @Override
-    public void autonomousInit()
-    {
+    public void autonomousInit() {
         autonomousCommand = robotContainer.getAutonomousCommand();
-        
-        if (autonomousCommand != null)
-        {
-            autonomousCommand.schedule();
-        }
+
+        if (autonomousCommand != null) autonomousCommand.schedule();
     }
-    
-    
+
     @Override
     public void autonomousPeriodic() {}
-    
-    
+
     @Override
     public void autonomousExit() {}
-    
-    
+
     @Override
-    public void teleopInit()
-    {
-        if (autonomousCommand != null)
-        {
+    public void teleopInit() {
+        if (autonomousCommand != null) {
             autonomousCommand.cancel();
         }
     }
-    
-    
+
     @Override
     public void teleopPeriodic() {}
-    
-    
+
     @Override
     public void teleopExit() {}
-    
-    
+
     @Override
-    public void testInit()
-    {
+    public void testInit() {
         CommandScheduler.getInstance().cancelAll();
     }
-    
-    
+
     @Override
     public void testPeriodic() {}
-    
-    
+
     @Override
     public void testExit() {}
 }
--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -9,20 +9,14 @@ import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
 
 
-
-public class RobotContainer
-{
-    public RobotContainer()
-    {
+public class RobotContainer {
+    public RobotContainer() {
         configureBindings();
     }
     
-    
     private void configureBindings() {}
     
-    
-    public Command getAutonomousCommand()
-    {
+    public Command getAutonomousCommand() {
         return Commands.print("No autonomous command configured");
     }
 }
