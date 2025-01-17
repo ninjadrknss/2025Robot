edcd785cea0b666e999aaa83297af34576acaf80
Alex <alex@petrys.com>
2025-01-16 19:08:32 -0800

    Swerve code generated!!!!

--- src/main/java/frc/robot/util/Constants.java
+++ src/main/java/frc/robot/util/Constants.java
@@ -4,5 +4,5 @@ public class Constants {
     /** Use {@see Robot.canivorebus #} instead */
     @SuppressWarnings("DeprecatedIsStillUsed")
     @Deprecated
-    public static final String canbus = "";
+    public static final String canbus = "Drivebase";
 }
