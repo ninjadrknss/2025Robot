2be53a72962e2d6f08ce7709230ce2ac1dcb4081
Alex <alex@petrys.com>
2025-01-25 11:36:10 -0800

    Adding distance sensor support for preemptive testing

--- src/main/java/frc/robot/subsystems/endeffector/EndEffectorConstants.java
+++ src/main/java/frc/robot/subsystems/endeffector/EndEffectorConstants.java
@@ -1,4 +1,10 @@
 package frc.robot.subsystems.endeffector;
 
+import au.grapplerobotics.interfaces.LaserCanInterface;
+
 public class EndEffectorConstants {
+    public final static int LaserCanID = 0;
+    public final static LaserCanInterface.RegionOfInterest roi = new LaserCanInterface.RegionOfInterest(
+            0, 0, 100, 100
+    );
 }
