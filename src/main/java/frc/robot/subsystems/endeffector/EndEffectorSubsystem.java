c50482c2778bdda1e9c47aa726a8e873c1a5c7d3
ninjadrknss <alex@petrys.com>
2025-01-26 10:17:37 -0800

    Revert "Merge branch 'main' of https://github.com/team751/2025Robot"
    This reverts commit ef97d4955883b3eb13cbd4eb7c1b3ddd45f87770, reversing
    changes made to 83681e0f518160ef87c64cd4dcb831f19458e0fa.

--- src/main/java/frc/robot/subsystems/endeffector/EndEffectorSubsystem.java
+++ src/main/java/frc/robot/subsystems/endeffector/EndEffectorSubsystem.java
@@ -1,16 +1,10 @@
 package frc.robot.subsystems.endeffector;
 
 
-import au.grapplerobotics.ConfigurationFailedException;
-import au.grapplerobotics.LaserCan;
-import au.grapplerobotics.interfaces.LaserCanInterface;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class EndEffectorSubsystem extends SubsystemBase {
     private static EndEffectorSubsystem instance;
-
-    private final LaserCan laserCan;
 
     public static EndEffectorSubsystem getInstance() {
         if (instance == null) instance = new EndEffectorSubsystem();
@@ -20,25 +14,6 @@ public class EndEffectorSubsystem extends SubsystemBase {
     private EndEffectorSubsystem() {
         // TODO: implement
         // Possible states? Spitting, Intaking, Idle
-
-        laserCan = new LaserCan(EndEffectorConstants.LaserCanID);
-
-        try {
-            laserCan.setRangingMode(LaserCan.RangingMode.SHORT);
-            laserCan.setRegionOfInterest(EndEffectorConstants.roi);
-            laserCan.setTimingBudget(LaserCanInterface.TimingBudget.TIMING_BUDGET_50MS);
-        } catch (ConfigurationFailedException e) {
-            System.err.println("Failed to configure laserCan");
-        }
-    }
-
-    @Override
-    public void periodic() {
-        SmartDashboard.putNumber("EndEffector/Distance", laserCan.getMeasurement().distance_mm);
-        SmartDashboard.putBoolean("EndEffector/Beam Broken", isBeamBroken());
-    }
-
-    public boolean isBeamBroken() {
-        return laserCan.getMeasurement().distance_mm < 100;
     }
 }
+
