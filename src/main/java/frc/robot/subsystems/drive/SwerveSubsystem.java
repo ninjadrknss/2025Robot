128a8d88019db345c3df7fc168f644a03b8665dc
Peter <89460630+Peter-Zhao-751@users.noreply.github.com>
2025-03-22 10:15:33 -0700

    im confused

--- src/main/java/frc/robot/subsystems/drive/SwerveSubsystem.java
+++ src/main/java/frc/robot/subsystems/drive/SwerveSubsystem.java
@@ -208,8 +208,8 @@ public class SwerveSubsystem extends TunerSwerveDrivetrain implements Subsystem 
     }
 
     public Pose2d getPose() {
-        return getState().Pose;
-        //return simDrivetrain == null ? getState().Pose : simDrivetrain.mapleSimDrive.getSimulatedDriveTrainPose();
+        //return getState().Pose;
+        return simDrivetrain == null ? getState().Pose : simDrivetrain.mapleSimDrive.getSimulatedDriveTrainPose();
     }
 
     public ChassisSpeeds getChassisSpeeds(){
